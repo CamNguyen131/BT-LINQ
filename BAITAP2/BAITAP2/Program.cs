a28ead60016b78bb1ccb878ef2b4abc2e5b12c81
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 13:42:01 +0700

    Lọc số lớn hơn 5

---
+++ BAITAP2/BAITAP2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai2
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        var result = numbers.Where(n => n > 5);
+
+        foreach (var n in result)
+            Console.Write(n + " ");
+    }
+}
