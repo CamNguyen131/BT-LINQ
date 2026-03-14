495893b96797e453644ffa2e16271a2247c0d879
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 14:47:59 +0700

    Tính tổng

---
+++ BAITAP16/BAITAP16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai16
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        var sum = numbers.Sum();
+
+        Console.ReadKey();
+    }
+}
