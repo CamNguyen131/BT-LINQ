6966f9ce1ffba41825a0d1849b5b4e4c9c66fc3b
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 14:46:33 +0700

    Kiểm tra có sinh viên rớt không

---
+++ BAITAP14/BAITAP14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai14
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public double Score;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="An",Score=8},
+            new Student{Id=2,Name="Binh",Score=4},
+            new Student{Id=3,Name="Chi",Score=9}
+        };
+
+        var fail = students.Any(s => s.Score < 5);
+
+        Console.ReadKey();
+    }
+}
