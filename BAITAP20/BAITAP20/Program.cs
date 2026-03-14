e656344e607ee86f89fc36c149b102dfdb2e19e2
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 14:50:45 +0700

    Top 3 sinh viên điểm cao nhất

---
+++ BAITAP20/BAITAP20/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai20
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public double Score;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="An",Score=8},
+            new Student{Id=2,Name="Binh",Score=6},
+            new Student{Id=3,Name="Chi",Score=9},
+            new Student{Id=4,Name="Dung",Score=7}
+        };
+
+        var top3 = students
+                    .OrderByDescending(s => s.Score)
+                    .Take(3);
+
+        Console.ReadKey();
+    }
+}
