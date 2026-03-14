3ef8851590967579027d56d47d8cd202584906a7
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 13:57:25 +0700

    Kiểm tra số >10

---
+++ BAITAP7/BAITAP7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai7
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        bool check = numbers.Any(n => n > 10);
+
+        Console.WriteLine("Co so > 10 khong: " + check);
+    }
+}
