f0c34b3774ff9f446a2133b519c270942ffa8e34
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 13:30:20 +0700

    Lọc số chẵn

---
+++ BAITAP1/BAITAP1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai1
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        var result = numbers.Where(n => n % 2 == 0);
+
+        Console.WriteLine("Cac so chan:");
+        foreach (var n in result)
+        {
+            Console.Write(n + " ");
+        }
+    }
+}
