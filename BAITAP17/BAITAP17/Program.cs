6ef0a87de8ffd829edb0fc72471786ee2bf8d8d9
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 14:48:42 +0700

    Tính trung bình

---
+++ BAITAP17/BAITAP17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai17
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public double Score;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="An",Score=8},
+            new Student{Id=2,Name="Binh",Score=6},
+            new Student{Id=3,Name="Chi",Score=9}
+        };
+
+        var avg = students.Average(s => s.Score);
+
+        Console.ReadKey();
+    }
+}
