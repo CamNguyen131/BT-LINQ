46833db980f0b2149405293b1e2ecbeaace38c7e
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 13:55:56 +0700

    Đếm số chẵn

---
+++ BAITAP6/BAITAP6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai6
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+        int count = numbers.Count(n => n % 2 == 0);
+
+        Console.WriteLine("So luong so chan: " + count);
+    }
+}
