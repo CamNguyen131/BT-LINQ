8af1955410f4a1318745170a70ade434b195cb34
CamNguyen131 <nguyentcnnl361511@gmail.com>
2026-03-14 13:36:26 +0700

    Update

--- BAITAP1/BAITAP1/Program.cs
+++ BAITAP1/BAITAP1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,14 +8,17 @@ class Bai1
     {
         Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
 
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+        List<int> numbers = new List<int>{1,2,3,4,5,6,7,8};
 
         var result = numbers.Where(n => n % 2 == 0);
 
         Console.WriteLine("Cac so chan:");
-        foreach (var n in result)
+
+        foreach(var n in result)
         {
             Console.Write(n + " ");
         }
+
+        Console.ReadKey();
     }
 }
