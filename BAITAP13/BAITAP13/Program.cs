eb5803c811acaa585c3a3cca7f78c8df8bedee6f
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 14:45:44 +0700

    Sinh viên đầu tiên >7

---
+++ BAITAP13/BAITAP13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai13
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public double Score;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="An",Score=8},
+            new Student{Id=2,Name="Binh",Score=6},
+            new Student{Id=3,Name="Chi",Score=9},
+            new Student{Id=4,Name="Dung",Score=7}
+        };
+
+        var first = students.FirstOrDefault(s => s.Score > 7);
+
+        Console.ReadKey();
+    }
+}
