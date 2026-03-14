d487cbdfa7283a52a1515ffb47423bce6fd94aa8
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 14:49:23 +0700

    Nhóm sinh viên theo xếp loại

---
+++ BAITAP18/BAITAP18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai18
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public double Score;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="An",Score=8},
+            new Student{Id=2,Name="Binh",Score=6},
+            new Student{Id=3,Name="Chi",Score=9}
+        };
+
+        var group = students.GroupBy(s => s.Score >= 8 ? "Gioi" : "Khac");
+
+        Console.ReadKey();
+    }
+}
