ad69dc576a75400576bec936fa3a6d5f6df011db
CamNguyen131 <nguyentcnnl361511@gmail.com>
2026-03-14 13:53:36 +0700

    Update Program.cs

--- BAITAP5/BAITAP5/Program.cs
+++ BAITAP5/BAITAP5/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,14 +8,13 @@ class Bai5
     {
         Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
 
-        List<int> numbers = new List<int> { 1, 2, 3 };
+        List<int> numbers = new List<int>{1,2,3};
 
         var result = numbers.Select(n => n * n);
 
-        Console.WriteLine("Binh phuong:");
-        foreach (var n in result)
-        {
+        foreach(var n in result)
             Console.Write(n + " ");
-        }
+
+        Console.ReadKey();
     }
 }
