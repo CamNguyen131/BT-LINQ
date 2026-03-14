a1366c50ae8f6ba0961ae5b15d49f5998ffbe0df
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 14:50:07 +0700

    Tìm số xuất hiện nhiều nhất

---
+++ BAITAP19/BAITAP19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai19
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 3, 3, 4 };
+
+        var most = numbers
+                    .GroupBy(x => x)
+                    .OrderByDescending(g => g.Count())
+                    .First();
+
+        Console.ReadKey();
+    }
+}
