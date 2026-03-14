31c4a0ab9d21a98893c9705a78d83c3e2f9146c3
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 13:45:44 +0700

    Sắp xếp tăng dần

---
+++ BAITAP3/BAITAP3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai3
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<int> numbers = new List<int> { 8, 3, 6, 1, 5 };
+
+        var result = numbers.OrderBy(n => n);
+
+        Console.WriteLine("Sap xep tang:");
+        foreach (var n in result)
+        {
+            Console.Write(n + " ");
+        }
+    }
+}
