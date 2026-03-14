9ec8577fafe2eee566a66b27e230aa1136d32629
CamNguyen131 <nguyentcnnl361511@gmail.com>
2026-03-14 15:00:53 +0700

    Update

--- BAITAP16/BAITAP16/Program.cs
+++ BAITAP16/BAITAP16/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,9 +8,11 @@ class Bai16
     {
         Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
 
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> numbers = new List<int>{1,2,3,4,5};
 
-        var sum = numbers.Sum();
+        int sum = numbers.Sum();
+
+        Console.WriteLine("Tong: " + sum);
 
         Console.ReadKey();
     }
