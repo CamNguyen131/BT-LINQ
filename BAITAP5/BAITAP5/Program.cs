123e62ef577af8682e85eeee7a72fd5474df1acf
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-14 13:52:31 +0700

    Bình phương

---
+++ BAITAP5/BAITAP5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai5
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
+
+        List<int> numbers = new List<int> { 1, 2, 3 };
+
+        var result = numbers.Select(n => n * n);
+
+        Console.WriteLine("Binh phuong:");
+        foreach (var n in result)
+        {
+            Console.Write(n + " ");
+        }
+    }
+}
