4ec3cd5ca39f25e3c56054b5e385ecf4b7aa631c
CamNguyen131 <nguyentcnnl361511@gmail.com>
2026-03-14 13:50:35 +0700

    Update

--- BAITAP4/BAITAP4/Program.cs
+++ BAITAP4/BAITAP4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,14 +8,13 @@ class Bai4
     {
         Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
 
-        List<int> numbers = new List<int> { 8, 3, 6, 1, 5 };
+        List<int> numbers = new List<int>{8,3,6,1,5};
 
         var result = numbers.OrderByDescending(n => n);
 
-        Console.WriteLine("Sap xep giam:");
-        foreach (var n in result)
-        {
+        foreach(var n in result)
             Console.Write(n + " ");
-        }
+
+        Console.ReadKey();
     }
 }
