b9d888737c24bcde0b104c982aece5987bfcf15d
CamNguyen131 <nguyentcnnl361511@gmail.com>
2026-03-14 15:00:21 +0700

    Sửa lỗi ko chạy

--- BAITAP15/BAITAP15/Program.cs
+++ BAITAP15/BAITAP15/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,9 +8,14 @@ class Bai15
     {
         Console.WriteLine("Nguyễn Thị Cẩm Nguyên - 23115053122328 - 225LTC#01");
 
-        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+        List<int> numbers = new List<int>{1,2,2,3,3,4,5};
 
         var result = numbers.Distinct();
+
+        foreach(var n in result)
+        {
+            Console.Write(n + " ");
+        }
 
         Console.ReadKey();
     }
